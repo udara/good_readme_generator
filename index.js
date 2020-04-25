1f0512d2d0cd23b9d61f69031f6d3880cfc2ea11
Udara ranasinghe <indi79@hotmail.com>
2020-04-25 14:03:21 +0930

    Adding basic file structure

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+
+const writeFileAsync = util.promisify(fs.writeFile);
