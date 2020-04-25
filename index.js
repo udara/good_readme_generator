123a43199bdab0862f1d7d1683bee387a3e387af
Udara ranasinghe <indi79@hotmail.com>
2020-04-25 14:17:35 +0930

    adding init()

--- index.js
+++ index.js
@@ -94,40 +94,41 @@ function instalationBadge(attribute,description,color)
 }
 
 function generateReadme(readme_input) {
-    return `# ${readme_input.project_title}
-    
-    ![](${readme_input.licence_badge})
-    
-    ${readme_input.description}
+return `# ${readme_input.project_title}
+
+![](${readme_input.licence_badge})
+
+${readme_input.description}
+   
+### Table of Contents
+1. Installation
+2. Usage
+3. License
+4. Contributing
+5. Tests
+6. Questions
        
-    ### Table of Contents
-    1. Installation
-    2. Usage
-    3. License
-    4. Contributing
-    5. Tests
-    6. Questions
-           
-    ### Installation 
-    ${readme_input.installation}
-          
-    ### Usage
-    ${readme_input.usage}
-    
-    ### License
-    ${readme_input.license}
-    
-    ### Contributing
-    ${readme_input.contributing}
-    
-    ### Tests
-    ${readme_input.tests}
-    
-    ### Questions
-    ${readme_input.github_username}
-    ${readme_input.github_email}
-    ${readme_input.github_profile}`;
-    }
+### Installation 
+${readme_input.installation}
+      
+### Usage
+${readme_input.usage}
+
+### License
+${readme_input.license}
+
+### Contributing
+${readme_input.contributing}
+
+### Tests
+${readme_input.tests}
+
+### Questions
+${readme_input.github_username}
+${readme_input.github_email}
+${readme_input.github_profile}`;
+}
+
 
 async function getGithubInfo(github_username,github_repo_name)
 {
@@ -142,3 +143,45 @@ async function getGithubInfo(github_username,github_repo_name)
         console.error('API ERROR: ' + e);
     }
 }
+
+
+async function writeReadme(readme_template)
+{  
+    try {
+    
+        await writeFileAsync("readme.md", readme_template);
+        console.log('SUCCESS: Readme.md file generated');
+        return true;
+    
+    }
+    catch(error) {
+
+        console.error('ERROR WHEN WRITING FILE: ' + error);
+        return false;
+
+    }
+}
+
+
+async function init()
+{
+    try {
+
+        const answers = await promptUser();
+        const github_info = await getGithubInfo(answers.github_username,answers.github_repo_name);
+            if(github_info)
+            {
+                let readme_input = {}
+                readme_input = answers;
+                readme_input.licence_badge = instalationBadge('License', answers.license,'brightgreen')
+                readme_input.github_profile = github_info.data.owner.avatar_url; 
+                const readme_template = generateReadme(readme_input);
+                const write_response = await writeReadme(readme_template);
+            }   
+      } catch(err) {
+        console.log(err);
+      }
+}
+
+init();
+
